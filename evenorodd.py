81a64f82a22ef5a0d1fd743409300e588acabd3a
prasanthkondapalli <59044278+prasanthkondapalli@users.noreply.github.com>
2020-02-06 19:42:26 +0530

    Add files via upload

---
+++ evenorodd.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  6 19:38:04 2020
+
+@author: Prasanth
+"""
+
+ a= int(input('enter the number a= >>'))
+if a%2==0:
+	print('Even')
+else:
+	print('Odd')
+	
